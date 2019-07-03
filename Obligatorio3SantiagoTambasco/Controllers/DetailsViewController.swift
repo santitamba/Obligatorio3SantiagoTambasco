6fc23a2967b3f0165707d2aec5a04e0ade4ac18d
Santiago <santiago.tambasco@gmail.com>
2019-07-03 00:02:46 -0300

    commit
    validacion de si alguien compro tu asiento

--- Obligatorio3SantiagoTambasco/Controllers/DetailsViewController.swift
+++ Obligatorio3SantiagoTambasco/Controllers/DetailsViewController.swift
@@ -21,6 +21,11 @@ class DetailsViewController: UIViewController {
     var asientos = [ZSeat]()
     var subtotal : Float?
     var final = ""
+    
+    var originRoom = SessionManager.room
+    var room = Room()
+    var selected = ""
+    var NewRoomMap = ""
     //var mapUpdate = SessionManager.selected_seats//"_AAAAAA_AAAAAA_AAAAAAAA_/_AAAAAA_AAAAAA_AUUUAAAA_/________________________/_AAAAAUUAAAUAAAAUAAAAAAA/_UAAUUUUUUUUUUUUUUUAAAAA/_AAAAAAAAAAAUUUUUUUAAAAA/_AAAAAAAAUAAAAUUUUAAAAAA/_AAAAAUUUAUAUAUAUUUAAAUU/"
     
 
@@ -51,16 +56,133 @@ class DetailsViewController: UIViewController {
         super.viewWillAppear(animated)
         for asiento in asientos{
             calculateMatrix(asiento: asiento)
+            calculateMatrixSelected(asiento: asiento)
         }
         print("este si \(final)")
         detailsViewController.reloadData()
         totalAmount()
+        
+        
+        //Para validar que nadie compre mi asiento
+        var documentId=SessionManager.room.docId!
+        db.collection("room").document(documentId)
+            .addSnapshotListener { documentSnapshot, error in
+                guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error!)")
+                    return
+                }
+                print("Current data: \(document.data())")
+                
+                let id = document.get("id") as! Int
+                let map = document.get("map") as! String
+                self.room.map=map
+                self.room.id=id
+                
+               
+                var originMatRoom = Array(self.selected)
+                var newMatRoom = Array(self.room.map!)
+                var cont = 0
+                for (e1, e2) in zip(originMatRoom, newMatRoom) {
+                    print("\(e1) - \(e2)")
+                    if e1=="S" && e2=="U"{
+                        var title = "Error"
+                        var message = "Acaban de comprar tu asiento"
+                        var buttonTitle = "Aceptar"
+                        let alert = UIAlertController(title: title, message: message,preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: buttonTitle,style: .default, handler: self.indexAlert1))
+                        self.present(alert, animated: true)
+                    }
+                    else{
+                        var NewRoomMap = self.room.map
+                        for asiento in self.asientos{
+                            self.calculateMat(asiento: asiento, map: NewRoomMap!)
+                        }
+
+                    }
+                }
+                //tengo que recorrer el original y todos los que ahora son nuevos U validarlos contra mis asientos si alguno coincide mandar un cartel de aviso y volver a la de seleccionar asientos
+        }
     }
+
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func indexAlert1(alert: UIAlertAction!){
+        SessionManager.deleteAllData()
+        //self.navigationController?.popViewController(animated: true)
+        
+        self.navigationController!.popToViewController(navigationController!.viewControllers[2], animated: false)
+        
+    }
+
+    
+    func calculateMat(asiento: ZSeat, map: String){
+        var val = 0
+        if asiento.row == 1{
+            val = (asiento.column-1)
+        }
+        else{
+            val = (((asiento.row-1)*24)+(asiento.column-1))+(asiento.row-1)
+        }
+        StringToMat(value: val,map: map )
+    }
+    
+    func StringToMat(value: Int, map: String){
+        var characters = Array(map)
+        characters[value] = "U"
+        //print(characters.count)
+        matrixToStringSel(matrix: characters)
+    }
+    
+    
+    func matrixToStringSel(matrix: [String.Element]) {
+        //var stringRepresentation = ""
+        var mapTemp = ""
+        for elem in matrix{
+            //stringRepresentation = elem.joined(separator:"")
+            //stringRepresentation = stringRepresentation + "/"
+            mapTemp = mapTemp + elem.description
+        }
+        final = mapTemp
+        //print ("acaa \(final) -----\n")
+    }
+    
+    func calculateMatrixSelected(asiento: ZSeat){
+        var val = 0
+        if asiento.row == 1{
+            val = (asiento.column-1)
+        }
+        else{
+            val = (((asiento.row-1)*24)+(asiento.column-1))+(asiento.row-1)
+        }
+        StringToMatrixSelected(value: val)
+    }
+    
+    func StringToMatrixSelected(value: Int){
+        var characters = Array(final)
+        characters[value] = "S"
+        //print(characters.count)
+        matrixToStringSelected(matrix: characters)
+    }
+    
+    
+    func matrixToStringSelected(matrix: [String.Element]) {
+        //var stringRepresentation = ""
+        var mapTemp = ""
+        for elem in matrix{
+            //stringRepresentation = elem.joined(separator:"")
+            //stringRepresentation = stringRepresentation + "/"
+            mapTemp = mapTemp + elem.description
+        }
+        selected = mapTemp
+        //print ("acaa \(final) -----\n")
+    }
+
     
     func calculateMatrix(asiento: ZSeat){
         var val = 0
